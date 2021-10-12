e74f3255589c099c18e95d5eda70428ea022a9ac
v1ntex <leonidt668@gmail.com>
2021-10-12 17:33:22 +0300

    First version with items

--- utils/fill_database/spiders/health.py
+++ utils/fill_database/spiders/health.py
@@ -1,6 +1,6 @@
 import scrapy
 from ..loaders import HealthLoader
-from scrapy.loader import ItemLoader
+
 
 class HealthSpider(scrapy.Spider):
     name = 'health'
@@ -18,27 +18,26 @@ class HealthSpider(scrapy.Spider):
             yield response.follow("https://health-diet.ru/base_of_food/food_24507/", callback=self.parse_kategories)
 
     def parse_kategories(self, response, **kwargs):
-        loader = ItemLoader(item=HealthLoader(), selector=response.xpath, response=response)
-        loader.add_xpath("product", '//*[contains(concat( " ", @class, " " ), concat( " ", "uk-table-condensed", " " ))]//a/text()')
-        loader.add_xpath("calories", '//td[(((count(preceding-sibling::*) + 1) = 2) and parent::*)]/text()')
-        loader.add_xpath('proteins','//td[(((count(preceding-sibling::*) + 1) = 3) and parent::*)]/text()')
-        loader.add_xpath('fats', '//td[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]/text()')
-        loader.add_xpath('carbohydrates','//td[(((count(preceding-sibling::*) + 1) = 5) and parent::*)]/text()')
-        loader.add_xpath('name_link','//h1/text()')
-        yield loader.load_item()
-        # for name_links in name_link:
-        #     name_links = name_links.split(' ')
-        #     name__link = name_links[::2]
-        # info = {
-        #     "product": product,
-        #     "calories": calories,
-        #     "proteins": proteins,
-        #     "fats": fats,
-        #     "carbohydrates": carbohydrates,
-        #     "link":  response.text,
-        #     "name_link": name__link
-        # }
-        # for key, value in info.items():
-        #     loader.add_value(key, value)
-        #
-        # info = loader.load_item()
+        loader = HealthLoader(response=response)
+        product = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "uk-table-condensed", " " ))]//a/text()').extract()
+        calories = response.xpath('//td[(((count(preceding-sibling::*) + 1) = 2) and parent::*)]/text()').extract()
+        proteins = response.xpath('//td[(((count(preceding-sibling::*) + 1) = 3) and parent::*)]/text()').extract()
+        fats = response.xpath('//td[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]/text()').extract()
+        carbohydrates = response.xpath('//td[(((count(preceding-sibling::*) + 1) = 5) and parent::*)]/text()').extract()
+        name_link = response.xpath('//h1/text()').extract()
+        for name_links in name_link:
+            name_links = name_links.split(' ')
+            name__link = name_links[::2]
+        info = {
+            "product": product,
+            "calories": calories,
+            "proteins": proteins,
+            "fats": fats,
+            "carbohydrates": carbohydrates,
+            "link":  response.text,
+            "name_link": name__link
+        }
+        for key, value in info.items():
+            loader.add_value(key, value)
+
+        info = loader.load_item()
