83e400616c3475f9f9e5c71c73c5b4828bb7eaf8
v1ntex <leonidt668@gmail.com>
2021-10-16 10:41:13 +0300

    handling of text

--- utils/fill_database/items.py
+++ utils/fill_database/items.py
@@ -2,9 +2,47 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+import scrapy
+# from scrapy.item import Item, Field
+from itemloaders.processors import MapCompose, Join, Identity, TakeFirst
 
-from scrapy.item import Item, Field
 
-class FillDatabaseItem(Item):
+def add_n(text: str) -> str:
+    try:
+        text = text.replace('",', '\n')
+    except ValueError as err:
+        print(err)
+    return text
+
+
+def change_text(text: str) -> str:
+    try:
+        text = text.split()
+    except ValueError as err:
+        print(err)
+    return text
+
+
+class FillDatabaseItem(scrapy.Item):
     # define the fields for your item here like:
-    pass
+    name = scrapy.Field(
+        input_processor=Join(),
+        output_processor=MapCompose(add_n),
+    )
+    call = scrapy.Field(
+        input_processor=MapCompose(change_text),
+        output_processor=Identity(),
+    )
+    proteins = scrapy.Field(
+        input_processor=MapCompose(change_text),
+        output_processor=Identity(),
+    )
+    fats = scrapy.Field(
+        input_processor=MapCompose(change_text),
+        output_processor=Identity(),
+    )
+    carboh = scrapy.Field(
+        input_processor=MapCompose(change_text),
+        output_processor=Identity(),
+    )
+
