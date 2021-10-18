258b01a36dcd99fae2b86ad64867d87379e7213a
v1ntex <leonidt668@gmail.com>
2021-10-18 19:44:19 +0300

    Created some models

---
+++ utils/dp_api/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey
+)
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'Users'
+    id = Column(String(100), nullable=False, unique=True)
