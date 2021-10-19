2f7417bd64756e6480b5b7fb0ab49c97b18eff78
v1ntex <leonidt668@gmail.com>
2021-10-19 17:18:47 +0300

    delete password field

--- states/registration_states.py
+++ states/registration_states.py
@@ -2,6 +2,5 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class Regisistration(StatesGroup):
-    password_registration = State()
     nickname_registration = State()
     finaly_registration = State()
