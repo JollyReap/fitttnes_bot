06365a7ae986188315c963ccfa48f71901d6f863
v1ntex <leonidt668@gmail.com>
2021-10-19 17:37:18 +0300

    created table for parse data

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
-    ForeignKey
 )
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,4 +13,13 @@ class Users(Base):
     id = Column(String(100), nullable=False, unique=True)
     email = Column(String(100), nullable=True, unique=True)
     nickname = Column(String(100), nullable=False, unique=True)
+
+
+class HealthDiet(Base):
+    __tablename__ = 'HealthDiet'
+    product_name = Column(String(100), nullable=False, unique=False)
+    calories = Column(String(100), nullable=True, unique=False)
+    proteins = Column(String(100), nullable=True, unique=False)
+    fats = Column(String(100), nullable=True, unique=False)
+    carboh = Column(String(100), nullable=True, unique=False)
 
