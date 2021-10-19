2850cac7874f37a05c760d7ecb1d6d5dc3f7340d
v1ntex <leonidt668@gmail.com>
2021-10-19 17:17:50 +0300

    Append new columns

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -8,6 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Users(Base):
     __tablename__ = 'Users'
     id = Column(String(100), nullable=False, unique=True)
+    email = Column(String(100), nullable=True, unique=True)
+    nickname = Column(String(100), nullable=False, unique=True)
+
