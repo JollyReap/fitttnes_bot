f69f0f1a8ce417a727d2a6bbc34a5f30ce3a6967
v1ntex <leonidt668@gmail.com>
2021-10-21 13:52:33 +0300

    Replaced string with varchar

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import (
     Column,
-    Integer,
+    VARCHAR,
     String,
 )
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,15 +11,15 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'Users'
     id = Column(String(100), nullable=False, unique=True)
-    email = Column(String(100), nullable=True, unique=True)
+    email = Column(VARCHAR(100), nullable=True, unique=True)
     nickname = Column(String(100), nullable=False, unique=True)
 
 
 class HealthDiet(Base):
     __tablename__ = 'HealthDiet'
     product_name = Column(String(100), nullable=False, unique=False)
-    calories = Column(String(100), nullable=True, unique=False)
-    proteins = Column(String(100), nullable=True, unique=False)
-    fats = Column(String(100), nullable=True, unique=False)
-    carboh = Column(String(100), nullable=True, unique=False)
+    calories = Column(VARCHAR(100), nullable=True, unique=False)
+    proteins = Column(VARCHAR(100), nullable=True, unique=False)
+    fats = Column(VARCHAR(100), nullable=True, unique=False)
+    carboh = Column(VARCHAR(100), nullable=True, unique=False)
 
