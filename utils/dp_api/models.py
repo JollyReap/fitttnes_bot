e300cc7ea01aa7cde23318f29841a3606fa823ed
v1ntex <leonidt668@gmail.com>
2021-11-01 12:50:53 +0300

    change type of tg_id

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     VARCHAR,
     String,
+    Integer
 )
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,7 +11,7 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'Users'
-    tg_id = Column(String(100), nullable=False, unique=True)
+    tg_id = Column(Integer, nullable=False, unique=True, primary_key=True)
     email = Column(VARCHAR(100), nullable=True, unique=True)
     nickname = Column(String(100), nullable=False, unique=True)
 
