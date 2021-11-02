e5e0f782cb9990d74ca574b9a00dd256774071c0
v1ntex <leonidt668@gmail.com>
2021-11-02 11:16:47 +0300

    changed little bit unessesary rows

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,8 +7,7 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!\n"
-                         f"Ты работаешь с проэктным ботом по информатике",
+    await message.answer(f"Привет, {message.from_user.full_name}!\n",
                          reply_markup=menu)
 
 
