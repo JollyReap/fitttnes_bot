388191e587902ef04a6aa2023b5b4def9292b000
v1ntex <leonidt668@gmail.com>
2021-11-08 12:32:09 +0300

    Wrote new notification

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,7 +8,8 @@ from data.config import ADMINS
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+            await dp.bot.send_message(admin, "Бот Запущен\n"
+                                             "Я вроде жив")
 
         except Exception as err:
             logging.exception(err)
