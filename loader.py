a43d53ba196bab50eaeec7c0673ac841b2c00414
v1ntex <leonidt668@gmail.com>
2021-11-08 12:31:38 +0300

    replaced instead of redis with memory

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-
+from utils.dp_api.database import Database
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2()
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+db = Database('sqlite:///data/users.db')
