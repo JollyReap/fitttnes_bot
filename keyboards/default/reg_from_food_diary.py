6bc1e5ba0071ef0d457ea8e6d9a6d9cb588ee37f
v1ntex <leonidt668@gmail.com>
2021-12-01 15:19:39 +0300

    forgot to add some args

--- keyboards/default/reg_from_food_diary.py
+++ keyboards/default/reg_from_food_diary.py
@@ -1,12 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-registration = ReplyKeyboardMarkup(
+registration = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True,
     keyboard=[
         [
-            KeyboardButton(text='Пройти сейчас')
-        ],
-        [
-            KeyboardButton(text='Позже сам(а) пройду')
+                KeyboardButton(text='Пройти сейчас')
+            ],
+            [
+                KeyboardButton(text='Позже сам(а) пройду')
+            ]
         ]
-    ]
 )
