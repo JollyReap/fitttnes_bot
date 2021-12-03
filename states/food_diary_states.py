986ac629d6ad73561b5f30d75d948722f5714edb
v1ntex <leonidt668@gmail.com>
2021-12-03 16:49:27 +0300

    add a new state

--- states/food_diary_states.py
+++ states/food_diary_states.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class Food_Diary(StatesGroup):
     product = State()
+    weight = State()
     save_in_db = State()
 
 # Can change
