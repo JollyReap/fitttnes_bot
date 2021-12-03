1ff466d97063ce33cb818555efe02f6829b4dae4
v1ntex <leonidt668@gmail.com>
2021-12-03 16:22:09 +0300

    added answer on button: 'назад'

--- handlers/users/food_diary.py
+++ handlers/users/food_diary.py
@@ -1,7 +1,7 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
+from keyboards.default.menu import menu
 from loader import dp, db
-from keyboards.default.reg_from_food_diary import registration
+from keyboards.default.food_diary_buttons import food_diary
 from keyboards.default.reg_button import button
 
 
@@ -10,8 +10,15 @@ async def check_status(message: types.Message):
     await message.answer('Проверяю есть ли ты в базе...')
     if db.select_user(message.from_user.id):
         await message.answer('Отлично! Ты зарегестрирован(а)\n'
-                             'Выбирай что будем делать😀')
+                             'Выбирай что будем делать😀',
+                             reply_markup=food_diary)
 
     else:
         await message.answer('Тебе надо зарегестрироваться чтобы вести дневник!\n'
                              'Хочешь сделать это сейчас?', reply_markup=button)
+
+
+@dp.message_handler(text='Назад⬅')
+async def back(message: types.Message):
+    await message.answer(text='...',
+                         reply_markup=menu)
