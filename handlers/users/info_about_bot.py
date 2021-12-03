584c6bd9d602535fdc3ec5869c54b20a6c88c511
v1ntex <leonidt668@gmail.com>
2021-12-03 16:23:13 +0300

    added reply_markup

--- handlers/users/info_about_bot.py
+++ handlers/users/info_about_bot.py
@@ -1,8 +1,9 @@
 from aiogram import types
-
+from keyboards.default.menu import menu
 from loader import dp
 
 
 @dp.message_handler(text="Информация о боте📖")
 async def info(message: types.Message):
-    await message.answer('Данный бот - проектная работа')
+    await message.answer('Данный бот - проектная работа',
+                         reply_markup=menu)
