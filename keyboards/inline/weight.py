009a6932bfe72e9b77f285949b4e36cdf5cf9480
v1ntex <leonidt668@gmail.com>
2021-12-09 15:16:05 +0300

    deleted uneccessary import

--- keyboards/inline/weight.py
+++ keyboards/inline/weight.py
@@ -1,15 +1,15 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from callback_dataes import weight_callback
+
 
 weight = InlineKeyboardMarkup(row_width=2,
                               inline_keyboard=[
                                   [
                                       InlineKeyboardButton(text='Да👍',
-                                                           callback_data=weight_callback.new('weight:true'))
+                                                           callback_data='weight:true')
                                   ],
                                   [
                                       InlineKeyboardButton(text='Нет👎',
-                                                           callback_data=weight_callback.new('weight:false'))
+                                                           callback_data='weight:false')
                                   ]
                               ]
                               )
