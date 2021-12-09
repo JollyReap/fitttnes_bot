f04f6e590b3c842f9b3ab19425589b219ca0fa49
v1ntex <leonidt668@gmail.com>
2021-12-09 15:01:25 +0300

    weight with callback_dataes

--- keyboards/inline/weight.py
+++ keyboards/inline/weight.py
@@ -1,12 +1,15 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from callback_dataes import weight_callback
 
 weight = InlineKeyboardMarkup(row_width=2,
                               inline_keyboard=[
                                   [
-                                      InlineKeyboardButton(text='Да👍')
+                                      InlineKeyboardButton(text='Да👍',
+                                                           callback_data=weight_callback.new('weight:true'))
                                   ],
                                   [
-                                      InlineKeyboardButton(text='Нет👎')
+                                      InlineKeyboardButton(text='Нет👎',
+                                                           callback_data=weight_callback.new('weight:false'))
                                   ]
-                              ])
+                              ]
+                              )
