1e10463a21c0079b4ad2b49cc53b7a1fae711e22
JollyReap <84176639+JollyReap@users.noreply.github.com>
2021-12-09 14:53:50 +0300

    unnecessary file

--- keyboards/inline/choose_state.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-true_or_false = InlineKeyboardMarkup(
-                row_width=1,
-                inline_keyboard=[
-                    InlineKeyboardButton
-                ]
-)
