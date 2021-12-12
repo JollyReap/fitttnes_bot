768124fe490b6f45fd8014b53088fdb867fb6365
v1ntex <leonidt668@gmail.com>
2021-12-12 15:37:43 +0300

    test version table for extracted data

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -2,6 +2,8 @@ from sqlalchemy import (
     Column,
     VARCHAR,
     String,
+    Integer,
+    Float
 )
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,4 +16,13 @@ class Users(Base):
     email = Column(VARCHAR(100), nullable=True, unique=True)
     nickname = Column(String(100), nullable=False, unique=True)
 
+
+class Products(Base):
+    __tablename__ = 'Products'
+    product_id = Column(Integer, nullable=False, unique=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    call = Column(Float, nullable=True)
+    proteins = Column(Float, nullable=True)
+    fats = Column(Float, nullable=True)
+    carboh = Column(Float, nullable=True)
 
