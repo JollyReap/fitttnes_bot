6cdc41a20dc8c0c877ce40c51f710c858df9f2d9
v1ntex <leonidt668@gmail.com>
2021-12-12 15:28:35 +0300

    keyboard for food_diary

--- keyboards/default/weight.py
+++ keyboards/default/weight.py
@@ -1,8 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-one_or_more  = ReplyKeyboardMarkup(row_width=2,
-                                   resize_keyboard=True,
-                                   keyboard=[
+one_or_more = ReplyKeyboardMarkup(row_width=2,
+                                  resize_keyboard=True,
+                                  one_time_keyboard=True,
+                                  keyboard=[
                                        [
                                            KeyboardButton(text='Одна')
                                        ],
