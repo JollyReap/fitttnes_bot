4bc3aa27e0476d8379da1953c863b785b6a83a80
v1ntex <leonidt668@gmail.com>
2021-12-12 14:45:22 +0300

    to begin with clear paper

--- handlers/users/enter_data.py
+++ handlers/users/enter_data.py
@@ -1,24 +1,9 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from loader import dp, db
-from keyboards.default.reg_from_food_diary import registration
+from keyboards.inline.weight import weight
+from keyboards.inline.callback_dataes import weight_callback
 from keyboards.default.snack_options import snack
 from states.food_diary_states import Food_Diary
 
-
-@dp.message_handler(text='Внести данные✍')
-async def enter_eating(message: types.Message):
-    await message.answer('Какой приём пищи хотите внести?',
-                         reply_markup=snack)
-
-    await Food_Diary.next()
-
-
-@dp.message_handler(state=Food_Diary.product)
-async def enter_product(message: types.Message, state: FSMContext):
-    await message.answer(f'{message.text}, хорошо!\n'
-                         f'Ведите что вы ели')
-
-    eating = await state.update_data(eating=message.text)
-    await Food_Diary.next()
 
