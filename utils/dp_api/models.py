0b30ab94a685d0b5c0c32805a7ccb5322b654278
v1ntex <leonidt668@gmail.com>
2021-12-14 16:49:11 +0300

    test verion models with columns for food_diary

--- utils/dp_api/models.py
+++ utils/dp_api/models.py
@@ -2,7 +2,8 @@ from sqlalchemy import (
     Column,
     VARCHAR,
     String,
-    Integer
+    Integer,
+    Float
 )
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,8 +12,21 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'Users'
-    tg_id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    tg_id = Column(String(100), nullable=False, unique=True, primary_key=True)
     email = Column(VARCHAR(100), nullable=True, unique=True)
     nickname = Column(String(100), nullable=False, unique=True)
+    snack = Column(String(30), nullable=False, unique=False)
+    product = Column(String, nullable=False, unique=False)
+    weight = Column(Float, nullable=True, unique=False)
+    time = Column(VARCHAR, nullable=False, unique=True)
 
+
+class Products(Base):
+    __tablename__ = 'Products'
+    product_id = Column(Integer, nullable=False, unique=True, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    call = Column(Float, nullable=True)
+    proteins = Column(Float, nullable=True)
+    fats = Column(Float, nullable=True)
+    carboh = Column(Float, nullable=True)
 
